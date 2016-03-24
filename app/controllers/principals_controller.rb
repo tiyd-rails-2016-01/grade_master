632d269ad1296f9d237d96c232bc68b927458160
Publicover <jameslpublicover@gmail.com>
2016-03-24 13:18:19 -0400

    remove everything but show, index and edit from controller

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -13,9 +13,7 @@ class PrincipalsController < ApplicationController
   end
 
   # GET /principals/new
-  def new
-    @principal = Principal.new
-  end
+  
 
   # GET /principals/1/edit
   def edit
@@ -23,43 +21,7 @@ class PrincipalsController < ApplicationController
 
   # POST /principals
   # POST /principals.json
-  def create
-    @principal = Principal.new(principal_params)
 
-    respond_to do |format|
-      if @principal.save
-        format.html { redirect_to @principal, notice: 'Principal was successfully created.' }
-        format.json { render :show, status: :created, location: @principal }
-      else
-        format.html { render :new }
-        format.json { render json: @principal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /principals/1
-  # PATCH/PUT /principals/1.json
-  def update
-    respond_to do |format|
-      if @principal.update(principal_params)
-        format.html { redirect_to @principal, notice: 'Principal was successfully updated.' }
-        format.json { render :show, status: :ok, location: @principal }
-      else
-        format.html { render :edit }
-        format.json { render json: @principal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /principals/1
-  # DELETE /principals/1.json
-  def destroy
-    @principal.destroy
-    respond_to do |format|
-      format.html { redirect_to principals_url, notice: 'Principal was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
