b5e10b10f1bfe1c9fa6a27518bf3efbe2a092e40
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-24 15:18:47 -0400

    added seeds for everyone back in

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,26 @@
 teacher = Teacher.create(first_name: "Brian", last_name: "Yarsawich")
 User.create(email: 'byarsawich@example.com', password: 'monkey', github_user_name: 'byarsawich', person_id: teacher.id, person_type: "Teacher")
 
-# teacher = Teacher.create(first_name: "Jefferson", last_name: "Neel")
-# User.create(email: 'jneel@example.com',password: 'monkey', github_user_name: 'eljefeasu', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'anjanamohanty@example.com', password: 'monkey', github_user_name: 'anjanamohanty', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'Publicover@example.com', password: 'monkey', github_user_name: 'Publicover', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'katestud@example.com', password: 'monkey', github_user_name: 'katestud', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'stevengcooper@example.com', password: 'monkey', github_user_name: 'stevengcooper', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'mkg1@example.com', password: 'monkey', github_user_name: 'mkg1', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: '@example.com', password: 'monkey', github_user_name: '', person_id: teacher.id, person_type: "Teacher")
-#
-# teacher = Teacher.create(first_name: "user", last_name: "user")
-# User.create(email: 'damianhouse@example.com', password: 'monkey', github_user_name: 'damianhouse', person_id: teacher.id, person_type: "Teacher")
+teacher = Teacher.create(first_name: "Jefferson", last_name: "Neel")
+User.create(email: 'jneel@example.com',password: 'monkey', github_user_name: 'eljefeasu', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'anjanamohanty@example.com', password: 'monkey', github_user_name: 'anjanamohanty', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'Publicover@example.com', password: 'monkey', github_user_name: 'Publicover', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'katestud@example.com', password: 'monkey', github_user_name: 'katestud', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'stevengcooper@example.com', password: 'monkey', github_user_name: 'stevengcooper', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'mkg1@example.com', password: 'monkey', github_user_name: 'mkg1', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: '@example.com', password: 'monkey', github_user_name: '', person_id: teacher.id, person_type: "Teacher")
+
+teacher = Teacher.create(first_name: "user", last_name: "user")
+User.create(email: 'damianhouse@example.com', password: 'monkey', github_user_name: 'damianhouse', person_id: teacher.id, person_type: "Teacher")
