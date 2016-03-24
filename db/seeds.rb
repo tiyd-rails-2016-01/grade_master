047120f57554eced9d76b0c18acd22107aae5d4a
Publicover <jameslpublicover@gmail.com>
2016-03-24 15:40:52 -0400

    add assignment date in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,4 @@ student = Student.create(first_name: "Ralph", last_name: "Wiggum", teacher_id: 1
 User.create(email: 'student@school.com', password: 'password', person_id: student.id, person_type: "Student")
 parent = Parent.create(first_name: "Chief", last_name: "Wiggum", student_id: 1)
 User.create(email: 'parent@school.com', password: 'password', person_id: parent.id, person_type: "Parent")
-grade = AssignmentGrade.create(grade: "C-", assignment_name: "Book Report", assignment_date: "3-24-2016", student_id: 1)
+grade = AssignmentGrade.create(grade: "C-", assignment_name: "Book Report", assignment_date: Time.now, student_id: 1)
