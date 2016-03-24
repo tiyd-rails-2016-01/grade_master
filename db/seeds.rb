bae1ec5f71a6ce0f7a4d32252f837f399ca404fb
Publicover <jameslpublicover@gmail.com>
2016-03-24 15:38:43 -0400

    put seeds in to test how awful a student Ralph Wiggum really is

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 teacher = Teacher.create(first_name: "Admin", last_name: "Teacher")
-User.create(email: 'admin@example.com', password: 'password', person_id: teacher.id, person_type: "Teacher")
+User.create(email: 'admin@example.com', password: 'password', person_id: 1, person_type: "Teacher")
 supernintendo_chalmers = Principal.new(first_name: "SN", last_name: "Chalmers")
-User.create(email: 'chalmers@school.com', password: 'password', person_id: "principal.id", person_type: "Principal")
+User.create(email: 'chalmers@school.com', password: 'password', person_id: 1, person_type: "Principal")
+student = Student.create(first_name: "Ralph", last_name: "Wiggum", teacher_id: 1)
+User.create(email: 'student@school.com', password: 'password', person_id: student.id, person_type: "Student")
+parent = Parent.create(first_name: "Chief", last_name: "Wiggum", student_id: 1)
+User.create(email: 'parent@school.com', password: 'password', person_id: parent.id, person_type: "Parent")
+grade = AssignmentGrade.create(grade: "C-", assignment_name: "Book Report", assignment_date: "3-24-2016", student_id: 1)
