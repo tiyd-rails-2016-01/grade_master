3fc4d22ad0ff3c18bafbe09bff59d2599617b70a
Steven Cooper <stevengcooper76@gmail.com>
2016-03-24 16:07:55 -0400

    README reflects all .pdf abilities in submitting a grade.

--- app/controllers/assignment_grades_controller.rb
+++ app/controllers/assignment_grades_controller.rb
@@ -46,7 +46,7 @@ class AssignmentGradesController < ApplicationController
   def update
     respond_to do |format|
       if @assignment_grade.update(assignment_grade_params)
-        format.html { redirect_to @assignment_grade, notice: 'Assignment grade was successfully updated.' }
+        format.html { redirect_to @assignment_grade, notice: "Assignment grade was successfully updated." }
         format.json { render :show, status: :ok, location: @assignment_grade }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ class AssignmentGradesController < ApplicationController
   def destroy
     @assignment_grade.destroy
     respond_to do |format|
-      format.html { redirect_to assignment_grades_url, notice: 'Assignment grade was successfully destroyed.' }
+      format.html { redirect_to assignment_grades_url, notice: "Assignment grade was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -75,7 +75,7 @@ class AssignmentGradesController < ApplicationController
     def assignment_grade_params
       params.require(:assignment_grade).permit(:grade, :assignment_name, :assignment_date, :student_id)
     end
-    ​
+
     def find_grades
       if session[:person_type] == "Teacher"
         AssignmentGrade.all
