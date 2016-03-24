9fc21c465ddefe0d81d1bca5721191afb9dbb86b
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-03-24 17:05:57 -0400

    Add validations for file size

--- app/models/assignment_grade.rb
+++ app/models/assignment_grade.rb
@@ -3,6 +3,7 @@ class AssignmentGrade < ActiveRecord::Base
   belongs_to :student
 
   validates_attachment_content_type :uploaded_file, :content_type => "application/pdf"
+  validates_attachment_size :uploaded_file, :less_than => 5.megabytes
   validates :assignment_name, presence: true
   validates :grade, presence: true
 end
