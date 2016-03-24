4fd4bf14da0ee6354d843298719f25dfc6258661
devdanlevy <daniel.levy2@iphone.com>
2016-03-24 15:49:50 -0400

    work on seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+student = Student.create(first_name: "eddie", last_name: "alvarez", teacher_id: 1)
 teacher = Teacher.create(first_name: "Admin", last_name: "Teacher")
+parent = Parent.create(first_name: "John", last_name: "Johnson", student_id: 1)
+student = Student.create(first_name: "Annie", last_name: "Anderson", teacher_id: 2)
+teacher = Teacher.create(first_name: "Mrs.", last_name: "Cranky")
+parent = Parent.create(first_name: "Dan", last_name: "Leveigh", student_id: 2)
+student = Student.create(first_name: "Anjana", last_name: "Smith", teacher_id: 3)
+teacher = Teacher.create(first_name: "Ricky", last_name: "Ricardo")
+parent = Parent.create(first_name: "Molly", last_name: "Ringworm", student_id: 3)
+
 User.create(email: 'admin@example.com', password: 'password', person_id: teacher.id, person_type: "Teacher")
+User.create(email: 'eddie@gmail.com', password: 'password', person_id: teacher.id, person_type: "Student")
+User.create(email: 'john@gmail.com', password: 'password', person_id: student.id, person_type: "Parent")
+User.create(email: 'mrs@gmail.com', password: 'password', person_id: teacher.id, person_type: "Teacher")
+User.create(email: 'annie@gmail.com', password: 'password', person_id: teacher.id, person_type: "Student")
+User.create(email: 'dan@gmail.com', password: 'password', person_id: student.id, person_type: "Parent")
+User.create(email: 'ricky@gmail.com', password: 'password', person_id: teacher.id, person_type: "Teacher")
+User.create(email: 'anjana@gmail.com', password: 'password', person_id: teacher.id, person_type: "Student")
+User.create(email: 'molly@gmail.com', password: 'password', person_id: student.id, person_type: "Parent")
