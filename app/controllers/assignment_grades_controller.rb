c8d7ed4e0227c68383a42631067f9f65de5d94b0
Kate Studwell <katestudwell@gmail.com>
2016-03-25 10:53:10 -0400

    Fix assignment grades controller to search for parent based on user id

--- app/controllers/assignment_grades_controller.rb
+++ app/controllers/assignment_grades_controller.rb
@@ -82,7 +82,7 @@ class AssignmentGradesController < ApplicationController
       elsif session[:person_type] == "Student"
         Student.find(User.find(session[:user_id]).person_id).assignment_grades
       else
-        @parent = Parent.find(session[:person_id])
+        @parent = Parent.find(User.find(session[:user_id]).person_id)
         @parent.assignment_grades
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160324175125) do
-
+ActiveRecord::Schema.define(version: 20160324190408) do
 
   create_table "assignment_grades", force: :cascade do |t|
     t.string   "grade"
