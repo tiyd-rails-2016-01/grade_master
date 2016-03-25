ad0f68d292d58554167ce96b39f2d8ab2a197584
devdanlevy <daniel.levy2@iphone.com>
2016-03-25 09:07:01 -0400

    student index page gets student average

--- test/models/assignment_grade_test.rb
+++ test/models/assignment_grade_test.rb
@@ -5,7 +5,13 @@ class AssignmentGradeTest < ActiveSupport::TestCase
     test "grade is a float and not a string" do
       assert_equal 168, assignment_grades(:one).grade + assignment_grades(:two).grade
     end
-# test "the truth" do
+
+    test "teacher sees student average grade on student index page" do
+    
+    end
+
+
+      # test "the truth" do
   #   assert true
   # end
 end
