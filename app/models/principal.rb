83320d71f87ae17c341e6def3d170693b5f747a5
Publicover <jameslpublicover@gmail.com>
2016-03-25 13:19:20 -0400

    new methods for dashboard stats

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -12,4 +12,28 @@ class Principal < ActiveRecord::Base
     @students = Student.all
     @students.count
   end
+
+  def self.display_student_grades(student)
+    AssignmentGrade.find_by(student_id: student.id).grade
+  end
+
+  def self.display_parent_first(student)
+    Parent.find_by(student_id: student.id).first_name
+  end
+
+  def self.display_parent_last(student)
+    Parent.find_by(student_id: student.id).last_name
+  end
+
+  def self.completed_assignments
+    @assignment_grade = AssignmentGrade.all
+    @assignment_grade.count
+  end
+
 end
+
+
+# define methods in principal model
+# make those methods self.
+# add them to the dashboard.show.html.erb
+# interpolate
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-teacher = Teacher.create(first_name: "Admin", last_name: "Teacher")
+teacher = Teacher.create(first_name: "Ms. Teresa", last_name: "Albright")
 User.create(email: 'admin@example.com', password: 'password', person_id: 1, person_type: "Teacher")
+
 supernintendo_chalmers = Principal.new(first_name: "SN", last_name: "Chalmers")
 User.create(email: 'chalmers@school.com', password: 'password', person_id: 1, person_type: "Principal")
+
 student = Student.create(first_name: "Ralph", last_name: "Wiggum", teacher_id: 1)
 User.create(email: 'student@school.com', password: 'password', person_id: student.id, person_type: "Student")
+
 parent = Parent.create(first_name: "Chief", last_name: "Wiggum", student_id: 1)
 User.create(email: 'parent@school.com', password: 'password', person_id: parent.id, person_type: "Parent")
-grade = AssignmentGrade.create(grade: "C-", assignment_name: "Book Report", assignment_date: Time.now, student_id: 1)
+
+grade = AssignmentGrade.create(grade: "C-", assignment_name: "Book Report", assignment_date: Time.now, student_id: 1
