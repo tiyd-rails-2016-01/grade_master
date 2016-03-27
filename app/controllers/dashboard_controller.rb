d64b81d30addd98dfa72fe362f614e8d7c73c96f
devdanlevy <daniel.levy2@iphone.com>
2016-03-27 16:04:14 -0400

    oh boy

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def show
     if session[:person_type] == "Student"
       @student = Student.find(User.find(session[:user_id]).person_id)
-    else
+    elsif session[:person_type] == "Parent"
       @student = Parent.find(User.find(session[:user_id]).person_id).student
     end
   end
