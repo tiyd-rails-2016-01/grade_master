22b1f6ed223fcb20fea0d7e752e6b533045f590e
Daniel Buompane <danielbuompane@gmail.com>
2016-03-28 15:36:49 -0400

    Refactor dashboard controll from .find to .find_by

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     if !session[:person_type]
       redirect_to login_path
     elsif session[:person_type] == "Student"
-      @student = Student.find(User.find(session[:user_id]).person_id)
+      @student = Student.find_by(User.find(session[:user_id]).person_id)
     elsif session[:person_type] == "Parent"
       @student = Parent.find(User.find(session[:user_id]).person_id).student
     end
